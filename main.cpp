d6c6784c0c37721893584dbd40dbe0a6bd3d13d9
Ivan <ivansannikov29@gmail.com>
2025-02-23 16:46:16 +0300

    Solution 2

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 #include <list>
 #include <string>
 #include <vector>
+#include <tuple>
 
 
 template <
@@ -38,6 +39,21 @@ void print_ip(Type v)
     std::cout << res << std::endl;
 }
 
+template <typename... Types>
+void print_ip(const std::tuple<Types...>& container)
+{
+    size_t tsz = std::tuple_size_v<std::tuple<Types...>>;
+    if (!tsz) return;
+    using mtype = std::tuple_element_t<0, std::tuple<Types...>>;
+    std::vector<mtype> res(tsz);
+    std::apply([&res](Types const&... tuple_args)
+        {
+            size_t id{0};
+            ((res[id++] = tuple_args), ...);
+        }, container);
+    std::cout << res << std::endl;
+}
+
 template <typename Container>
 auto print_ip(const Container& container) -> decltype((void)container.size())
 {
@@ -53,5 +69,6 @@ int main()
     print_ip(std::string{"Hello, World!"}); // Hello, World!
     print_ip(std::vector<int>{100, 200, 300, 400}); // 100.200.300.400
     print_ip(std::list<short>{400, 300, 200, 100}); // 400.300.200.100
+    print_ip(std::make_tuple(123, 456, 789, 0)); // 123.456.789.0
     return 0;
 }
